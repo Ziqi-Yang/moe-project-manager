fcfb26a29990e1c9014ec45cbd9e8233bb38a1c9
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2023-03-08 08:00:37 +0800

    style: Args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use clap::{ArgGroup, Args, Parser, Subcommand};
 use std::path::PathBuf;
-use clap::{Args, Parser, Subcommand, ArgGroup};
 
 /// A Extraordinary Moe Global Project Manager
 #[derive(Parser, Debug)]
@@ -14,8 +14,7 @@ struct Arg {
     command: Option<Commands>,
 }
 
-#[derive(Debug)]
-#[derive(Subcommand)]
+#[derive(Debug, Subcommand)]
 enum Commands {
     /// add project root to known projects list
     Add {
@@ -26,50 +25,78 @@ enum Commands {
 
         /// target program language, defautl to autodetect language
         #[arg(short, long)]
-        language: Option<String>,  
+        language: Option<String>,
 
         /// add category attribution
         #[arg(short, long)]
-        category: Option<String>,  
+        category: Option<String>,
 
         /// add tag attribution, can be used multiple times
         // -t hello -t hi
         #[arg(short, long)]
-        tags: Vec<String>,  
+        tags: Vec<String>,
     },
 
-    /// Scan all subdirectories and automatically add projects 
-    Scan {
-        path: PathBuf,
-    },
-    
+    /// Scan all subdirectories and automatically add projects
+    Scan { path: PathBuf },
 
     /// change directory, but also check whether project existence and prompt you to add it
     // don't prompt if already prompted
-    Cd {
-        path: PathBuf,
-    },
+    Cd { path: PathBuf },
 
     /// init project using project tempates
     Init {
         /// target program language, if language is not set, the range is all languages
         #[arg(short, long)]
-        language: Option<String>,  
+        language: Option<String>,
 
         /// choose a template in corresponding language(or all languages if language is not set)
         #[arg(short, long)]
         template: Option<String>,
 
         /// don't use fzf, just list templates plainly
-        #[arg(short='f', long)]
+        #[arg(short = 'f', long)]
         no_fzf: bool,
     },
-    
+
     /// List existing projects
-    List(Filter),
+    List {
+        /// [filter] target program language, default to all languages
+        #[arg(short, long)]
+        language: Option<String>,
+
+        /// [filter] certain category
+        #[arg(short, long)]
+        category: Option<String>,
+
+        /// [filter] certain tags, can be used multiple times
+        // -t hello -t hi
+        #[arg(short, long)]
+        tags: Vec<String>,
+
+        /// list dependencies
+        dependencies: bool,
+    },
 
     /// jump into a project root in projects list, default using fzf
-    Jump(FilterFzf),
+    Jump {
+        /// [filter] target program language, default to all languages
+        #[arg(short, long)]
+        language: Option<String>,
+
+        /// [filter] certain category
+        #[arg(short, long)]
+        category: Option<String>,
+
+        /// [filter] certain tags, can be use multiple times
+        // -t hello -t hi
+        #[arg(short, long)]
+        tags: Vec<String>,
+
+        /// don't use fzf, just list templates plainly
+        #[arg(short = 'f', long)]
+        no_fzf: bool,
+    },
 
     /// edit configuration or projects attribution using default editor
     #[command(group(
@@ -85,45 +112,7 @@ enum Commands {
         /// configuration
         #[arg(short)]
         configuration: bool,
-    }
-}
-
-
-
-#[derive(Args, Debug)]
-struct Filter {
-    /// [filter] target program language, default to all languages
-    #[arg(short, long)]
-    language: Option<String>,  
-
-    /// [filter] certain category
-    #[arg(short, long)]
-    category: Option<String>,  
-
-    /// [filter] certain tags, can be used multiple times
-    // -t hello -t hi
-    #[arg(short, long)]
-    tags: Vec<String>,  
-}
-
-#[derive(Args, Debug)]
-struct FilterFzf {
-    /// [filter] target program language, default to all languages
-    #[arg(short, long)]
-    language: Option<String>,  
-
-    /// [filter] certain category
-    #[arg(short, long)]
-    category: Option<String>,  
-
-    /// [filter] certain tags, can be use multiple times
-    // -t hello -t hi
-    #[arg(short, long)]
-    tags: Vec<String>,  
-
-    /// don't use fzf, just list templates plainly
-    #[arg(short='f', long)]
-    no_fzf: bool,
+    },
 }
 
 fn main() {
