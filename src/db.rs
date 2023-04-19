345c4482f7c1a2e16de6911e320c17f92f407436
Zarkli Leonardo <mr.ziqiyang@gmail.com>
2023-04-19 15:16:58 +0800

    feat: db load and save funtions

--- src/db.rs
+++ src/db.rs
@@ -1,25 +1,52 @@
-use anyhow::Result;
+use std::fs;
+use std::io::ErrorKind;
+use std::io::prelude::*;
 use std::path::PathBuf;
+use anyhow::Result;
+use serde::{Serialize, Deserialize};
 use crate::config;
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Entry((PathBuf, Option<String>, Option<String>, Option<String>, Vec<String>));
 
-pub struct DataBase {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Database {
     path: PathBuf,
     entries: Vec<Entry>
 }
 
-impl DataBase{
-    pub fn new() -> Result<Self> {
+impl Database{
+    fn new() -> Result<Self> {
         let path = config::get_data_dir()?.join(config::DB_NAME);
-        Ok(DataBase{
+        Ok(Database {
             path,
             entries: Vec::new()
         })
     }
 
+    pub fn get_or_create_db() -> Result<Self> {
+        let path = config::get_data_dir()?.join(config::DB_NAME);
+        match fs::read(path) {
+            Ok(content) => {
+                let decoded: Database = bincode::deserialize(&content).unwrap();
+                Ok(decoded)
+            },
+            Err(e) if e.kind() == ErrorKind::NotFound => {
+                Database::new()
+            },
+            Err(e) => Err(e.into())
+        }
+    }
+
     pub fn add(&mut self, project_path: PathBuf, nick_name: Option<String>, language: Option<String>, category: Option<String>, tags: Vec<String>) {
         self.entries.push(Entry((project_path, nick_name, language, category, tags)))
+    }
+
+    pub fn save_to_disk(&self) -> Result<()> {
+        let encoded: Vec<u8> = bincode::serialize(self).unwrap();
+        let mut file = fs::File::create(self.path.to_str().unwrap())?;
+        file.write_all(&encoded)?;
+        Ok(())
     }
 }
 
@@ -29,12 +56,12 @@ mod test {
 
     #[test]
     fn db_initialize_test() {
-        let _ = DataBase::new().unwrap();
+        let _ = Database::new().unwrap();
     }
 
     #[test]
     fn db_add_test() {
-        let mut db = DataBase::new().unwrap();
+        let mut db = Database::new().unwrap();
         db.add(PathBuf::from("/tmp"), None, None, None, vec!());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,9 @@ use cmd::{Arg};
 
 fn main() -> Result<()> {
     let cli = Arg::parse();
-    cli.check_arg()?;
+    // cli.check_arg()?;
     // cli.run()?;
+    // let db = db::Database::get_or_create_db()?;
+    // db.save_to_disk();
     Ok(())
 }
